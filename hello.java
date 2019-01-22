3d9a5e4f8ef81ecb26db36b699666e57928d04ab
kuriakosej183 <kuriakosej183@ALB205-11.strose.edu>
2019-01-22 15:43:18 -0500

    Creating the hello program

---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+import.java.util.Scanner; 
+public class Hello {
+
+	public static void main(String[] args){
+		
+		Scanner x = new Scanner.(system.in);
+		String name = x.nextLine()
+		System.out.println("The Name is:" + name ); 
+		
+	}
+}
