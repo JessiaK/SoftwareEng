70decf10204976b96187ba8581a8c101876261e2
JessiaK <jessyk520@gmail.com>
2019-01-23 16:23:01 -0500

    Hello.hs

--- hello.java
+++ hello.java
@@ -1,13 +1,15 @@
+/*
+This is a simple program that outputs the name of the user 
+that runs the program. 
+*/
 import java.util.Scanner;
-
-import.java.util.Scanner; 
 public class Hello {
 
 	public static void main(String[] args){
 		
-		Scanner x = new Scanner.(system.in);
-		String name = x.nextLine()
-		System.out.println("The Name is:" + name ); 
+		Scanner x = new Scanner.(system.in);  //scanner being declared and initialized
+		String name = x.nextLine()  //name would be the input from user
+		System.out.println("The Name is:" + name ); //getting the name printed
 		
 	}
 }
