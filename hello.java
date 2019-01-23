a1a932064c302d69f5c442f711bbfae0346edb1e
thomasm825 <thomasm825@strose.edu>
2019-01-23 16:49:10 -0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -9,7 +9,7 @@ public class Hello {
 		
 		Scanner x = new Scanner.(system.in);  //scanner being declared and initialized
 		String name = x.nextLine()  //name would be the input from user
-		System.out.println("The Name is:" + name ); //getting the name printed
+		System.out.println(name + "is the user that is running this program."); //getting the name printed
 		
 	}
 }
